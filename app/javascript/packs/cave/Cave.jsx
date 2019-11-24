832a2cee70de30ae412a3833568d69fad04f9a63
karrakis <karrakis@gmail.com>
2019-11-23 21:45:10 -0600

    extract caveGenerator code and start setting up the recursive pathing

--- app/javascript/packs/cave/Cave.jsx
+++ app/javascript/packs/cave/Cave.jsx
@@ -1,49 +1,15 @@
 import React from "react";
 import Grid from "./Grid";
 
-const GRID_WIDTH = 11;
-const GRID_HEIGHT = 11;
-
-const randInRow = () => {
-  return Math.floor(Math.random() * GRID_WIDTH);
-};
-
-const randInCol = () => {
-  // 11 * 11 is 121 which is the highest
-  return Math.floor(Math.random() * GRID_HEIGHT);
-};
-
-const makeRightExit = data => {
-  data[GRID_WIDTH * randInCol() + 10] = 1;
-  return data;
-};
-
-const makeLeftExit = data => {
-  data[GRID_WIDTH * randInCol()] = 1;
-  return data;
-};
-
-const makeTopExit = data => {
-  data[randInRow()] = 1;
-  return data;
-};
-
-const makeBotExit = data => {
-  data[randInRow() + (GRID_HEIGHT * GRID_WIDTH - GRID_WIDTH)] = 1;
-  return data;
-};
-
-const generateCaves = data => {
-  data = makeTopExit(data);
-  data = makeRightExit(data);
-  data = makeBotExit(data);
-  data = makeLeftExit(data);
-  return data;
-};
+import {
+  generateCave,
+  GRID_WIDTH,
+  GRID_HEIGHT
+} from "../../../../lib/caveGenerator/caveGenerator";
 
 const Cave = props => {
   var data = new Array(GRID_WIDTH * GRID_HEIGHT).fill(0);
-  var data = generateCaves(data);
+  var data = generateCave(data);
   return (
     <div className="w-screen h-screen bg-black flex items-center justify-center relative">
       <Grid patternData={data} />
