46c07a1f564bc8a812f98b50046d99330effd270
karrakis <karrakis@gmail.com>
2019-11-28 06:51:29 -0600

    refactor Grid.jsx

--- app/javascript/packs/cave/Grid.jsx
+++ app/javascript/packs/cave/Grid.jsx
@@ -2,20 +2,11 @@ import React from "react";
 import Row from "./Row";
 
 const Grid = ({ patternData }) => {
+  const rows = patternData.map(
+    row => <Row>{row}</Row> //your boat
+  );
   return (
-    <div className="flex flex-col w-container h-screen bg-blue-900">
-      <Row>{patternData[0]}</Row>
-      <Row>{patternData[1]}</Row>
-      <Row>{patternData[2]}</Row>
-      <Row>{patternData[3]}</Row>
-      <Row>{patternData[4]}</Row>
-      <Row>{patternData[5]}</Row>
-      <Row>{patternData[6]}</Row>
-      <Row>{patternData[7]}</Row>
-      <Row>{patternData[8]}</Row>
-      <Row>{patternData[9]}</Row>
-      <Row>{patternData[10]}</Row>
-    </div>
+    <div className="flex flex-col w-container h-screen bg-black">{rows}</div>
   );
 };
 
